c5cc653137b8d0ed6545e948713b8d1328894b1e
Moi Petitnoel <moivp4@gmail.com>
2020-02-14 07:09:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,8 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  my_hash_creator = { key: "value" }
+  my_hash_creator1 = { key: "value" }
+  return my_hash_creator1
 end
 
 def read_from_hash(hash, key)
